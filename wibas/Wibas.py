d9f83a9ec35a72329dc1aa044caf2156c6f0f5b4
AnandaRauf <uzumaki.nanda@rocketmail.com>
2020-11-24 22:39:25 +0700

    Wibas new library packages Python

---
+++ wibas/Wibas.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+from flask import render_template
+import jinja2
+import pyttsx3 
+from kivy.app import App
+from kivy.uix.gridlayout import Gridlayout
+from kivy.properties import NumericProperty
+from kivy.properties import StringProperty
+import pyautogui
+import pynput
